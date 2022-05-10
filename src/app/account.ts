925f78b1fb6e573f9643b221b6caaaca86a4ce80
Mahalakshmi <mahalakshmi.bollam@gmail.com>
2022-05-10 12:56:12 +0530

    final commit 1.0

---
+++ src/app/account.ts
@@ -0,0 +1,7 @@
+export class Account {
+
+    empId: Number = 0;
+  accountId: String | any;
+  branch: String | any;
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { UtilityComponent } from './utility/utility.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { TeamComponent } from './team/team.component';
+
+const routes: Routes = [
+  { path:'',component:HomeComponent},
+  { path:'util',component:UtilityComponent},
+  { path:'Maha',component:AboutComponent},
+  { path:'contact',component:ContactComponent},
+  { path:'employee',component:EmployeeComponent},
+  { path:'team',component:TeamComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
